4d45a58575c2c111497c72e9897328902ad9ffb4
aniawiki <120105518+aniawiki@users.noreply.github.com>
2022-12-16 13:36:29 +0000

    zadanie

--- kolko1/Baza_danych.cpp
+++ kolko1/Baza_danych.cpp
@@ -4,26 +4,27 @@ using namespace std;
 
 int main() {
     ios_base::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
+    cin.tie(0); cout.tie(0);
 
     int n, p;
-    long k;
-    map<long, int> m;
-    map<long, int>::iterator it;
+    long long k;
+    map<long long, int> m;
     cin >> n;
     for (int i = 0; i < n; i++) {
         cin >> k >> p;
-        it = m.find(k);
-        if (it == m.end() && m[k] + p >= 0) {
-            m[k] = p;
-            cout << p << "\n";
+        if (m.find(k) == m.end()) {
+            if (p >= 0) {
+                m[k] = p;
+                cout << p << "\n";
+            }
+            else
+                cout << "NIE\n";
         }
         else if (m[k] + p < 0)
             cout << "NIE\n";
-        else if (it != m.end() && m[k] + p >= 0) {
+        else if (m[k] + p >= 0) {
             m[k] += p;
             cout << m[k] << "\n";
         }
     }
-}
+}   
