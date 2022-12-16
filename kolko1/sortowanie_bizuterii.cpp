02f9ea3ced2ff8628bbd0f1e7f2b0037637801fe
aniawiki <120105518+aniawiki@users.noreply.github.com>
2022-12-16 16:47:48 +0100

    zadanie

--- kolko1/sortowanie_bizuterii.cpp
+++ kolko1/sortowanie_bizuterii.cpp
@@ -5,7 +5,12 @@ using namespace std;
 const int MAXN = 1e6+7;
 pair <int, string> arr[MAXN];
 int n;
-string str;
+
+bool Sort(const pair<int, string> &a, const pair<int, string> &b) {
+    if (a.first != b.first)
+        (return a.first < b.first)
+    return (a.second < b.second)
+}
 
 int main() {
     ios_base::sync_with_stdio(0);
@@ -16,7 +21,7 @@ int main() {
         cin >> str;
         arr[i] = {str.length(), str};
     }
-    sort(arr, arr+n);
+    sort(arr, arr+n, Sort);
     for (int i = 0; i < n; i++)
         cout << arr[i].second << "\n";
 }
