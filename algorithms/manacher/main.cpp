9b5aacd810e8b65ecf4b5fc28a0c485ae181570f
aniawiki <annsie21@wp.pl>
2023-04-08 10:50:46 +0200

    radius of palindroms in each place (odd and even)

---
+++ algorithms/manacher/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void manacher(string s, int n) {
+    vector<int> d1(n), d2(n);
+    for (int i = 0, l = 0, r = -1; i < n; i++) {
+        int k = (i > r) ? 1 : min(d1[l + r - i], r - i + 1);
+        while (0 <= i - k && i + k < n && s[i - k] == s[i + k]) k++;
+        d1[i] = k--;
+        if (i + k > r) {
+            l = i - k;
+            r = i + k;
+        }
+    }
+    for (int i = 0, l = 0, r = -1; i < n; i++) {
+        int k = (i > r) ? 0 : min(d2[l + r - i + 1], r - i + 1);
+        while (0 <= i - k - 1 && i + k < n && s[i - k - 1] == s[i + k]) k++;
+        d2[i] = k--;
+        if (i + k > r) {
+            l = i - k - 1;
+            r = i + k ;
+        }
+    }
+    for (int i = 0; i < n; i++) cout << d1[i] << " ";
+    cout << "\n";
+    for (int i = 1; i < n; i++) cout << d2[i] << " ";
+}
+
+int main() {
+    int n;
+    string s;
+    cin >> n >> s;
+    manacher(s, n);
+}
