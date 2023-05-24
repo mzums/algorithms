6b11f208e8111d7a1513ec6528e12edb93f6e490
aniawiki <annsie21@wp.pl>
2023-05-24 21:02:34 +0200

    easy, sum pref

---
+++ lessons/drzewa/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+const int MAXN = 1e6+7;
+int b[MAXN];
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    string str;
+    int n, x, y;
+    cin >> str >> n;
+    str = " " + str;
+
+    for (int i = 1; i < str.length(); i++) {
+        if (str[i] == 'b') b[i]++;
+        b[i] += b[i-1];
+    }
+    for (int i = 0; i < n; i++) {
+        cin >> x >> y;
+        cout << b[y] - b[x-1] << "\n";
+    }
+}
