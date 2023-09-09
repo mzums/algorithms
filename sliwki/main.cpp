39d2ff243f95fda2e9a94bc5b792d159b566f4a9
mzums <annsie035@gmail.com>
2023-09-09 13:19:19 +0200

    point-interval tree

--- sliwki/main.cpp
+++ sliwki/main.cpp
@@ -1,77 +1,82 @@
 #include <iostream>
 using namespace std;
 
-const int base = 8;
-long long tree[2*base+7][7], tree2[2*base+7];
-long long n, q, p, c, l, r, k, res, v1;
-string op;
+const int BASE = 1 << 17;
+int arr[BASE+7];
+int tree[2*BASE+7][7];
 
-void upd(int v, int wart) {
-    v += base;
-    tree2[v] += wart;
-    for (int i = 1; i <= 6; i++) {
-        if (tree2[v] % i == 0)
-            tree[v][i] = 1;
-        else tree[v][i] = 0;
-    }
-    v /= 2;
-    v1 = v;
-    for (int i = 1; i <= 6; i++) {
-        v = v1;
-        while (v > 0) {
-            tree[v][i] = tree[2*v][i] + tree[2*v+1][i];
-            v /= 2;
-        }
+void init() {
+    for (int i = BASE; i <= 2*BASE; i++)
+        for (int j = 1; j <= 5; j++) 
+            tree[i][j] = 1;
+
+    for (int i = BASE-1; i >= 1; i--) {
+        for (int j = 1; j <= 5; j++) 
+            tree[i][j] = tree[2*i][j] + tree[2*i+1][j];
     }
 }
 
-int query(int a, int b, int x) {
-    res = 0;
-    a += (base-1);
-    b += (base+1);
+void upd(int x, int layer) {
+    while(x > 0) {
+        tree[x][layer] = tree[2*x][layer] + tree[2*x+1][layer];
+        x /= 2;
+    }
+}
 
-    while (a/2 != b/2) {
-        if (a%2==0) res += tree[a+1][x];
-        if (b%2==1) res += tree[b-1][x];
-        a /= 2;
-        b /= 2;
+int query(int a, int b, int c) {
+    int res = 0;
+    a += BASE - 1;
+    b += BASE - 1;
+    while (a <= b) {
+        if (a % 2 == 1) {
+            res += tree[a][c];
+            ++a;
+        }
+        if (b % 2 == 0) {
+            res += tree[b][c];
+            --b;
+        }
+        a >>= 1;
+        b >>= 1;
     }
     return res;
 }
 
 int main() {
     ios_base::sync_with_stdio(0);
-    cin.tie(0); cout.tie(0);
+    cin.tie(0);
 
+    init();
+
+    string op;
+    int n, q, a, b, c, a2;
     cin >> n >> q;
-    for (int i = 2*base; i > base; i--)
-        for (int j = 1; j <= 6; j++) {
-            if (tree2[i] % j == 0)
-                tree[i][j] = 1;
-            else tree[i][j] = 0;
-        }
-
-    for (int v = 2*base; v > base; v--) {
-        for (int i = 1; i <= 6; i++) {
-            v = v1;
-            while (v > 0) {
-                tree[v][i] = tree[2*v][i] + tree[2*v+1][i];
-                v /= 2;
-            }
-        }
-    }
-    for (int i = base+1; i <= 2*base; i++) cout << tree[i][2] << " ";
-    cout << "\n";
-    
     for (int i = 0; i < q; i++) {
         cin >> op;
         if (op == "DODAJ") {
-            cin >> p >> c;
-            upd(p, c);
+            cin >> a >> b;
+            arr[a] += b;
+            a2 = a+BASE-1;
+            for (int j = 1; j <= 5; j++) {
+                if (arr[a] % j == 0) tree[a2][j] = 1;
+                else tree[a2][j] = 0;
+                upd(a2/2, j);
+            }
         }
         else {
-            cin >> l >> r >> k;
-            cout << query(l, r, k) << "\n";
+            cin >> a >> b >> c;
+            cout << query(a, b, c) << "\n";
         }
     }
 }
+/*
+5 8
+DODAJ 2 4
+DODAJ 5 10
+DODAJ 3 4
+SKRZYNKI 2 5 1
+SKRZYNKI 2 5 1
+DODAJ 4 7
+SKRZYNKI 2 5 1
+SKRZYNKI 2 3 1
+*/
