1ea889c7e826ef5dbfa1c554323d56a4ba825b29
mzums <annsie035@gmail.com>
2023-09-27 22:39:36 +0200

    interesting, difficult, idea, recursion

--- wwi2023/level_2_5/jedynki/main.cpp
+++ wwi2023/level_2_5/jedynki/main.cpp
@@ -1,12 +1,12 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 string solve(int n) {
     if (n == 0) return "";
     if (n == 1) return "1";
     if (n == 2) return "(1+1)";
-    if (n % 2 == 0) return "((1+1)*"+solve(n/2)+")";
-    if(n%2==1) return "(1+"+solve(n-1)+")";
+    if (n % 2 == 0) return "((1+1)*" + solve(n/2) + ")";
+    if (n % 2 == 1) return "(1+" + solve(n-1) + ")";
 } 
 
 int main(){
