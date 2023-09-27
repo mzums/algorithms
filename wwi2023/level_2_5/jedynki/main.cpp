89d2d1050e57769e10f65a4dc76b5f07e15e32fa
mzums <annsie035@gmail.com>
2023-09-27 22:38:04 +0200

    interesting, difficult, idea, recursion

---
+++ wwi2023/level_2_5/jedynki/main.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+string solve(int n) {
+    if (n == 0) return "";
+    if (n == 1) return "1";
+    if (n == 2) return "(1+1)";
+    if (n % 2 == 0) return "((1+1)*"+solve(n/2)+")";
+    if(n%2==1) return "(1+"+solve(n-1)+")";
+} 
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n, t;
+    cin >> n;
+    while (n--){
+        cin >> t;
+        cout << solve(t) << '\n';
+    }
+}
