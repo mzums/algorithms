80b22543eed8a96e2bf3a02aa9a3830943853a6d
mzums <annsie035@gmail.com>
2024-04-22 17:42:17 +0200

    double hashing, 100 points

--- lessons/y_2023_24/przydzielanie_adresow/main.cpp
+++ lessons/y_2023_24/przydzielanie_adresow/main.cpp
@@ -4,20 +4,15 @@ using namespace std;
 typedef long long ll;
 
 constexpr int MAXN = 1e5+7;
-constexpr int P = 17;
+constexpr int P = 2141, P2 = 1523;
 constexpr int MOD = 1e9+7;
-int H[MAXN], H2[MAXN], powP[MAXN];
+int H[MAXN], H2[MAXN], H3[MAXN], H4[MAXN], powP[MAXN], powP2[MAXN];
 string pat[MAXN];
-set<int> s;
+set<int> s, s2;
 
-int get_hash1(int l, int r, int n) {
-    int res = ((ll)H[r] - H[l-1] + MOD) % MOD;
-    return ((ll)res * powP[n-l+1]) % MOD;
-}
-
-int get_hash2(int l, int r, int n) {
-    int res = ((ll)H2[r] - H2[l-1] + MOD) % MOD;
-    return ((ll)res * powP[n-l+1]) % MOD;
+int get_hash(int l, int r, int n, int* Hx, int* powPx) {
+    int res = ((ll)Hx[r] - Hx[l-1] + MOD) % MOD;
+    return ((ll)res * powPx[n-l+1]) % MOD;
 }
 
 void solve(int k, int n) {
@@ -28,13 +23,16 @@ void solve(int k, int n) {
     int m = pat[0].length();
 
     for (int i = m-1; i <= n; ++i) {
-        s.insert(get_hash1(i-m+2, i, n));
+        s.insert(get_hash(i-m+2, i, n, H, powP));
+        s2.insert(get_hash(i-m+2, i, n, H3, powP2));
     }
     for (int i = 0; i < k; ++i) {
         for (int j = 1; j < m; ++j) {
             H2[j] = ((ll)H2[j-1] + (ll)powP[j] * pat[i][j]) % MOD;
+            H4[j] = ((ll)H4[j-1] + (ll)powP2[j] * pat[i][j]) % MOD;
         }
-        if (s.find(get_hash2(1, m-1, n)) != s.end()) cout << "OK\n";
+        if (s.find(get_hash(1, m-1, n, H2, powP)) != s.end()
+        && s2.find(get_hash(1, m-1, n, H4, powP2)) != s2.end()) cout << "OK\n";
         else cout << "NIE\n";
     }
 }
@@ -50,10 +48,13 @@ int main() {
     str = ' ' + str;
     n = str.length() - 1;
     powP[0] = 1;
+    powP2[0] = 1;
 
     for (int i = 1; i <= n; ++i) {
         powP[i] = ((ll)powP[i-1] * P) % MOD;
+        powP2[i] = ((ll)powP2[i-1] * P2) % MOD;
         H[i] = ((ll)H[i-1] + (ll)powP[i] * str[i]) % MOD;
+        H3[i] = ((ll)H3[i-1] + (ll)powP2[i] * str[i]) % MOD;
     }
     cin >> t;
     for (int i = 0; i < t; ++i) {
